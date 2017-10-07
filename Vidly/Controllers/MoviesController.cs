6ca7b1c3e3c4240e575e6c84c118d9e419413706
Edersio <edemlove@gmail.com>
2017-10-07 00:44:08 -0400

    Add model, view and controller files

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,16 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek!"};
-            return View (movie);
+           return View (movie);
+          
         }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:range(1,12)}")]
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
     }
 }
